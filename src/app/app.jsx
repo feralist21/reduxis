c8e4703baf81dd567b7abc1448ae370764e4a855
Alexandr Dementev <ad@adictgroup.ru>
2024-06-14 15:30:57 +0800

    Добавляет пример хранилища

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,7 +1,11 @@
+import { Counter } from "../features/counter/Counter";
+
+
 const App = () => {
     return (
         <div>
             <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+            <Counter/>
         </div>
     );
 };
---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,22 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlise';
+
+export function Counter() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <div>
+                <button aria-label='Decrement value' onClick={() => dispatch(decrement())}>
+                    Decrement
+                </button>
+                <span>{count}</span>
+
+                <button aria-label='Increment value' onClick={() => dispatch(increment())}>
+                    Increment
+                </button>
+            </div>
+        </div>
+    );
+}
