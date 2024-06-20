7c345bdc2d27cce8b0c6308c52f44c95cc3eaa68
Alexandr Dementev <ad@adictgroup.ru>
2024-06-20 10:54:55 +0800

    Добавляет пример RTK query

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,11 +1,12 @@
 import { Counter } from "../features/counter/Counter";
-
+import Pokemon from "../features/pokemon/Pokemon";
 
 const App = () => {
     return (
         <div>
             <h1 className='text-3xl font-bold underline'>Hello world!</h1>
             <Counter/>
+            <Pokemon/>
         </div>
     );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { counterSlice} from '../features/counter/counterSlice';
+import { counterSlice } from '../features/counter/counterSlice';
+import { pokemonApi } from '../services/pokemon';
 
 const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
+        [pokemonApi.reducerPath]: pokemonApi.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 export default store;
